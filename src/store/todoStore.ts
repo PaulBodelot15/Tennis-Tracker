4fa36407d66a333e60ad3ff2f2b9f35260bd35c9
PaulBodelot15 <paul15062004@gmail.com>
2025-06-06 10:46:47 +0200

    chore: ensure newline at EOF

--- src/store/todoStore.ts
+++ src/store/todoStore.ts
@@ -69,3 +69,4 @@ export const useTodoStore = create<TodoState>((set) => ({
     });
   },
 }));
+
--- src/store/trainingStore.ts
+++ src/store/trainingStore.ts
@@ -48,3 +48,4 @@ export const useTrainingStore = create<TrainingState>((set) => ({
     });
   },
 }));
+
