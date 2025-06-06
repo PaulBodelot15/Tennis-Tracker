b8101b009580f50c39fe8aa44dca544194e6bda8
PaulBodelot15 <paul15062004@gmail.com>
2025-06-06 10:45:59 +0200

    Use basename prop in BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import Statistics from './pages/Statistics';
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Dashboard />} />
